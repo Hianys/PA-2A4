0c5b1d952d86cfd0a04a5f9faa31fe1802528199
Fabio D. <fabio.dugay@gmail.com>
2025-05-13 22:45:39 +0200

    Début de la partie admin

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -30,7 +30,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Mot de passe de confirmation')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmation du mot de passe')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -57,11 +57,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('Déjà inscrit ?') }}
             </a>
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __("S'inscrire") }}
             </x-primary-button>
         </div>
     </form>
--- resources/views/dashboards/admin.blade.php
+++ resources/views/dashboards/admin.blade.php
@@ -1,12 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold">
-            Tableau de bord administrateur
-        </h2>
+        <h2 class="text-xl font-semibold">Tableau de bord administrateur</h2>
     </x-slot>
 
-    <div class="py-12 px-6">
+    <x-admin-content>
+        <h2 class="text-2xl font-semibold mb-4">Gestion des utilisateurs</h2>
         <p>Bienvenue, {{ Auth::user()->name }}.</p>
-        <p>En tant qu’administrateur, vous pouvez gérer les utilisateurs et consulter les différents espaces.</p>
-    </div>
+    </x-admin-content>
 </x-app-layout>
